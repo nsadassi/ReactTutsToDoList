cd6a10b269b0bee4f5f65300aea8dc2cc106cb39
nikhillesh <nikhillesh94@gmail.com>
2020-10-29 19:00:37 -0700

    pushing react to github

---
+++ src/ListItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ListItem.css';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import FlipMove from 'react-flip-move';
+
+function ListItem(props){
+    const items = props.items;
+    const listItems = items.map(item =>{
+        return <div className="list" key={item.key}>
+            <p>
+                <input type="text" id={item.key} value={item.text} onChange={(e) => props.setUpdate(e.target.value, item.key)}/>
+                <span><DeleteForeverIcon className="trash" onClick={() => props.deleteItem(item.key)}/></span>
+            </p>
+        </div>
+    });
+    return(
+        <div>
+           <FlipMove duration={300} easing="ease-in-out">{listItems}</FlipMove> 
+        </div>
+    );
+}
+
+export default ListItem;
